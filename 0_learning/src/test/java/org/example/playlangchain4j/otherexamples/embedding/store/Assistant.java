e29a727941f40de05f2d22ed27545bc3dfcdb670
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-05-27 17:29:20 +0200

    trying to use ollama for local Java files. -> Produces timeout

--- 0_learning/src/test/java/org/example/playlangchain4j/otherexamples/embedding/store/Assistant.java
+++ 0_learning/src/test/java/org/example/playlangchain4j/otherexamples/embedding/store/Assistant.java
@@ -1,6 +1,7 @@
 package org.example.playlangchain4j.otherexamples.embedding.store;
 
 import dev.langchain4j.service.Result;
+import dev.langchain4j.service.TokenStream;
 
 public interface Assistant {
 	Result<String> chat(String userMessage);
