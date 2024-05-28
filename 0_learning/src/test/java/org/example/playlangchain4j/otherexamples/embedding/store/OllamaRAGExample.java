70e2d02743669fae1d2484ec3b6b881f884956cb
Peter Kullmann <peter.kullmann@me.com>
2024-05-28 11:07:19 +0200

    Try RAG with native ollama -- with streaming api

--- 0_learning/src/test/java/org/example/playlangchain4j/otherexamples/embedding/store/OllamaRAGExample.java
+++ 0_learning/src/test/java/org/example/playlangchain4j/otherexamples/embedding/store/OllamaRAGExample.java
@@ -4,10 +4,11 @@ import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.TokenStream;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
@@ -17,7 +18,7 @@ public class OllamaRAGExample {
 
     interface Assistant {
 
-        String chat(String userMessage);
+        TokenStream chat(String userMessage);
     }
 
     public static void main(String[] args) {
@@ -26,19 +27,22 @@ public class OllamaRAGExample {
         InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
         EmbeddingStoreIngestor.ingest(documents, embeddingStore);
 
-        ChatLanguageModel model = OllamaChatModel.builder()
+        StreamingChatLanguageModel model = OllamaStreamingChatModel.builder()
                 .baseUrl("http://localhost:11434")
                 .modelName("codellama")
                 .temperature(1.0)
                 .build();
 
         Assistant assistant = AiServices.builder(Assistant.class)
-                .chatLanguageModel(model)
+                .streamingChatLanguageModel(model)
                 .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
                 .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
                 .build();
 
-        String answer = assistant.chat("How to do Easy RAG with LangChain4j?");
-        System.out.println(answer);
+        TokenStream tokenStream = assistant.chat("Convert a nonogram to a sudoku");
+        tokenStream.onNext(System.out::print)
+                .onComplete(System.out::println)
+                .onError(Throwable::printStackTrace)
+                .start();
     }
 }
